47e3f2e2a421e9f7512a38d48f0c3a6551243a8c
amtoine <44101798+amtoine@users.noreply.github.com>
2022-10-15 10:35:26 +0200

    add install and clean scripts as well as a config

---
+++ clean.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+source config.sh
+
+if [ -d "$venv" ]; then
+  $RM -rf "$venv"
+else
+  echo "$venv is not installed..."
+fi
+
+if [ -f "$bin/$exe" ]; then
+  $RM "$bin/$exe"
+else
+  echo "$bin/$exe is not installed..."
+fi
+
+if [ -f "$exe" ]; then
+  $RM "$exe"
+else
+  echo "$exe not built..."
+fi
---
+++ config.sh
@@ -0,0 +1,5 @@
+venv="$VIRTUALENVWRAPPER_HOOK_DIR/remote-PS3"
+exe="remote"
+bin="$HOME/.local/bin"
+
+RM="rm --verbose"
