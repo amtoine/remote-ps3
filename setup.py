3aa255df92deb194605b094400fd3854a9b47d5c
amtoine <44101798+amtoine@users.noreply.github.com>
2022-10-22 13:15:54 +0200

    replace pyproject.toml by setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    include_package_data=True,
+    name='remote',
+    version='0.1.0',
+    description='',
+    url="https://github.com/amtoine/remote-PS3",
+    author='',
+    author_email='',
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    long_description='',
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
