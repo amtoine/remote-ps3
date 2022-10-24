0d0e1c1aa7597b34df4bcf71a2068493f8fcd48b
amtoine <44101798+amtoine@users.noreply.github.com>
2022-10-24 19:01:29 +0200

    add documentation to the utils module

--- remote/utils.py
+++ remote/utils.py
@@ -13,10 +13,13 @@ def get_config_with_profile(profile: str, *, filename: str) -> Config:
     with open(filename, "r") as remote_config_file:
         config = json.load(remote_config_file)
 
+    # merge the common config and the profile-specifi config
+    # into a single one
     common = config["common"]
     profile_config = config["profiles"][profile]
     profile_config.update(common)
 
+    # notify the user to see the change on the screen, without the terminal
     notification.notify(
         title="PS3 remote.py",
         message=f"Profile: {profile}",
