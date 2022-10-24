8cae6c7b6922a6a7e780f775b88e5966572c76d1
amtoine <44101798+amtoine@users.noreply.github.com>
2022-10-24 19:12:33 +0200

    refactor and add documentation to the state module

--- remote/state.py
+++ remote/state.py
@@ -1,10 +1,24 @@
 from dataclasses import dataclass
-from typing import Dict
+from typing import Any, Dict
 
 import evdev
 
 AXIS_CENTER = 128
 AXIS_OFF_CENTER_TOLERANCE = 50
+
+
+def is_axis_off_center(value: int) -> bool:
+    return abs(value - AXIS_CENTER) > AXIS_OFF_CENTER_TOLERANCE
+
+
+def compute_normalized_axis_offset(value: int) -> float:
+    return (value - AXIS_CENTER) / AXIS_CENTER
+
+
+def remove_none_from_dict(dictionary: Dict[Any, Any]) -> Dict[Any, Any]:
+    return {
+        key: value for key, value in dictionary.items() if value is not None
+    }
 
 
 @dataclass
@@ -15,30 +29,37 @@ class ControllerState:
     axes: Dict[str, int]
 
     def update_keys(self, event: evdev.events.KeyEvent) -> None:
+        """Update the keys of the controller based on an event."""
         keys = evdev.ecodes.keys[event.scancode]
-        key_name = (
-            sorted(keys, key=lambda x: len(x))[1] if isinstance(keys, list) else keys
-        )
+        # get the appropriate key state
+        if isinstance(keys, list):
+            key_name = sorted(keys, key=lambda x: len(x))[1]
+        else:
+            key_name = keys
+        # and set the internal controller key to the keystate
         self.keys[key_name] = event.keystate
 
     def update_axis(self, event: evdev.events.AbsEvent) -> None:
-        self.axes[evdev.ecodes.ABS[event.event.code]] = event.event.value
+        """Update the axes of the controller based on an event."""
+        axis = evdev.ecodes.ABS[event.event.code]
+        self.axes[axis] = event.event.value
 
     def get_down_keys(self):
+        """Get the list of all the keys currently held down."""
         return [key for key, pressed in self.keys.items() if pressed]
 
     def get_off_center_axes(self):
+        """Get the list of off-centered axes, with the normalized offset."""
         return [
-            (axis, (value - AXIS_CENTER) / AXIS_CENTER)
+            (axis, compute_normalized_axis_offset(value))
             for axis, value in self.axes.items()
-            if value is not None
-            and abs(value - AXIS_CENTER) > AXIS_OFF_CENTER_TOLERANCE
+            if value is not None and is_axis_off_center()
         ]
 
     def __repr__(self, *, ommit_none: bool = False) -> str:
         if ommit_none:
-            keys = {key: value for key, value in self.keys.items() if value is not None}
-            axes = {key: value for key, value in self.axes.items() if value is not None}
+            keys = remove_none_from_dict(self.keys)
+            axes = remove_none_from_dict(self.axes)
         else:
             keys = self.keys
             axes = self.axes
