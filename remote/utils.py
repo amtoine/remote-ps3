b6ac6b88a4d18690ad3f553931596b04131b1698
amtoine <44101798+amtoine@users.noreply.github.com>
2022-10-24 18:31:28 +0200

    remove useless tool functions

--- remote/utils.py
+++ remote/utils.py
@@ -6,23 +6,6 @@ import json
 PROFILES = ["qutebrowser", "mpv", "firefox"]
 
 
-def scale(val, src, dst):
-    """
-    Scale the given value from the scale of src to the scale of dst.
-
-    val: float or int
-    src: tuple
-    dst: tuple
-
-    example: print(scale(99, (0.0, 99.0), (-1.0, +1.0)))
-    """
-    return (float(val - src[0]) / (src[1] - src[0])) * (dst[1] - dst[0]) + dst[0]
-
-
-def scale_stick(value):
-    return scale(value, (0, 255), (-100, 100))
-
-
 def get_config_with_profile(profile: str, *, filename: str) -> Dict[str, Any]:
     """Extract a profile from the global configuration."""
     with open(filename, "r") as remote_config_file:
